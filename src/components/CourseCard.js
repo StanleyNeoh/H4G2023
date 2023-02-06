971882fd9c6df6735f732a475ce5d5c1fd1e10bc
Thanh Chu <lilyhanky57@gmail.com>
2023-02-06 21:24:11 +0800

    Add general course card component

---
+++ src/components/CourseCard.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Rating from '@mui/material/Rating';
+import { CardActionArea } from '@mui/material';
+
+const CourseCard = ({title, image, description, rating}) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          sx={{ height: 140 }}
+          image={image}
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+          <Rating disabled name="size-medium" defaultValue={0} value={rating}/>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Enroll</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+export default CourseCard;
