b20f9c94c5d25ba954b38a8f158e59104b22ccd1
Steven Lim <limhwsteven@gmail.com>
2023-02-11 01:45:45 +0800

    Fix cors issue in backend

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
 const app = require('express')();
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 var userRouter = require('./routes/users');
 var courseRouter = require('./routes/course');
 
-app.use('/users', userRouter);
-app.use('/courses', courseRouter);
+app.use('/users', cors(), userRouter);
+app.use('/courses', cors(), courseRouter);
 
 exports.api = functions.https.onRequest(app);
